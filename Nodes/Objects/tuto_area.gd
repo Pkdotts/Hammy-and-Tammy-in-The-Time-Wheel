c933655d6d4ea839f85dbad1c60a96e4d9259493
JumpmanFR <jumpman@jumpman.fr>
2025-08-03 01:57:01 +0200

    Tutorial

---
+++ Nodes/Objects/tuto_area.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+@export_multiline var text: String = ""
+
+func _ready() -> void:
+	$Label.text = text
+	$Label.modulate.a = 0.0
+
+func _on_body_entered(body: Node2D) -> void:
+	if body is Player:
+		var tween = get_tree().create_tween()
+		tween.tween_property($Label, "modulate:a", 1.0, 0.5)
+
+func _on_body_exited(body: Node2D) -> void:
+	if body is Player:
+		var tween = get_tree().create_tween()
+		tween.tween_property($Label, "modulate:a", 0.0, 0.5)
--- Scripts/exit_hole.gd
+++ Scripts/exit_hole.gd
@@ -2,6 +2,9 @@ extends Sprite2D
 
 @export var backwards_door := false
 
+func _ready() -> void:
+	$Label.modulate.a = 0.0
+
 func _is_active():
 	return (Global.got_cheese == backwards_door)
 
@@ -11,12 +14,14 @@ func _input(event: InputEvent) -> void:
 			Global.get_current_level().end_level()
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body == Global.current_hammy:
-		Global.current_hammy.in_front_of_door = true
-		$Label.visible = true
+	if body is Player and _is_active():
+		body.in_front_of_door = true
+		var tween = get_tree().create_tween()
+		tween.tween_property($Label, "modulate:a", 1.0, 0.5)
 
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
-	if body == Global.current_hammy:
-		Global.current_hammy.in_front_of_door = false
-		$Label.visible = false
+	if body is Player and _is_active():
+		body.in_front_of_door = false
+		var tween = get_tree().create_tween()
+		tween.tween_property($Label, "modulate:a", 0.0, 0.5)
