0588828738a77c4a711e22a0f8f3d676d0bbdfb5
JumpmanFR <jumpman@jumpman.fr>
2025-08-03 16:51:18 +0200

    Fixed level 1 checkpoint

--- Scripts/check_point.gd
+++ Scripts/check_point.gd
@@ -1,8 +1,12 @@
 extends Area2D
 
+@export var _enabled_on_normal := true
+@export var _enabled_on_reverted := true
+
 func _ready() -> void:
 	connect("body_entered", _on_body_entered)
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
-		Global.get_current_level().hit_checkpoint(self)
+		if (Global.got_cheese and _enabled_on_reverted) or (not Global.got_cheese and _enabled_on_normal):
+			Global.get_current_level().hit_checkpoint(self)
