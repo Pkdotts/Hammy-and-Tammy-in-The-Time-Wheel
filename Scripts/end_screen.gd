6eba80e19c9b337eea40221b3f4dfd98029844fd
Danionel <122308508+DanionelP@users.noreply.github.com>
2025-08-03 13:18:42 +0200

    improved platform movement

--- Scripts/end_screen.gd
+++ Scripts/end_screen.gd
@@ -4,7 +4,6 @@ var finished = false
 
 func _ready() -> void:
 	_play_anim()
-	#$AnimationPlayer.queue("FadeOut")
 
 func _process(_delta: float) -> void:
 	if finished and (Input.is_action_just_pressed("ui_accept") or Input.is_action_just_pressed("ui_cancel")):
